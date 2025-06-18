8d64a496fd845f3404f715679d4c0d34944e2de4
udaykumbhani999 <uday.k@simformsolutions.com>
2025-06-18 20:16:08 +0530

    nexcent set up done t4 version add

---
+++ nexcent/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <Button variant="default">Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
